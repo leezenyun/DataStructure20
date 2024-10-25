ae79f1334f1eb3dcf37d9b9c94f962c756be1ed7
leezenyun <lee940531gm@gmail.com>
2024-10-25 13:41:41 +0800

    20241025one

--- 2dAry4html.js
+++ 2dAry4html.js
@@ -1,18 +1,25 @@
-var ary2d=[
-    [1,1,0,1,1], //index 0
-    [1,0,1,1,0], //index 1
-    [1,0,1,0,0], //index 2
-    [1,0,1,1,0], //index 3
-    [1,0,1,0,0]   //index 4
-];
+var ary2d=[50][50]
+
+for(var j=0;j<=50;j++){
+    for(var k=0;k<=50;k++){
+        var rand = Math.floor(Math.random()*1);
+        
+        ary2d[j][k]= rand;
+    }
+
+}
+//max _row=0...4, _col=0...5
+// [1][2]=>
+// [0][1],[0][2],[0][3]     =>[_row-1][_col-1],[_row-1][_col],[_row-1][_col+1]
+// [1][1],[_row][_col],[1][3]=>[_row][_col-1],[_row][_col],[_row][_col+1]
+// [2][1],[2][2],[2][3]      =>[_row+1][_col-1],[_row+1][_col],[_row+1][_col+1]
 
 var row=ary2d.length;
 var col=ary2d[0].length;
 
 //draw map
 var canvas = document.getElementById("map").getContext("2d");
-var sizewidth=canvas.Elementwidth/row;
-var sizehigh=canvas.Elementhigh/col;
+var size=Math.min(canvas.canvas.height/row, canvas.canvas.width/col);
 for(var _row=0;_row<row;_row++){
     for(var _col=0;_col<col;_col++){
         //ar2d[_row][_col]=>0,1
@@ -21,8 +28,16 @@ for(var _row=0;_row<row;_row++){
         }else{
             canvas.fillStyle="#000000"
         }
-        //600/5=>120  coordinate, width, heigth
-        canvas.fillRect(_row*120,_col*120,sizewidth,sizehigh);
-        canvas.strokeRect(_row*120,_col*120,sizewidth,sizehigh);
+        //600/5=>120  coordinate x,y , width, heigth
+        canvas.fillRect(_col*size,_row*size,size,size);
+        canvas.strokeRect(_col*size,_row*size,size,size);
     }
+}
+
+function neighbor(_row,_col){
+   var count=0;
+   //visit 8 neighbor
+
+   return count;
+
 }
